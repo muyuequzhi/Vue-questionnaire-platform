8abc1f44d474c3d1cee83a8d9da30c95fdd53b09
MertensMing <13680543450@163.com>
2016-09-19 15:01:56 +0800

    update

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -8,18 +8,13 @@
 export default {
   data () {
     return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
       msg: 'Hello World!'
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h1 {
   color: #42b983;
 }
